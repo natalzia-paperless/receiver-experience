870824d7fb6dbae60294466bffe60bebf1bc6be4
Joe Natalzia <jnatalzia@paperlesspost.com>
2015-02-09 13:37:31 -0500

    Relayout with correct gradients

--- src/javascripts/app.js
+++ src/javascripts/app.js
@@ -3,15 +3,12 @@ $(function(){
   var img = new Image();
   img.src = src;
   img.onload = function() {
-    setTimeout(function() {
-      
-      $('.loader').animate({opacity:0}, function() {
-        $(this).remove();
-        $('.card-image-holder').addClass('animate-in');
-        $('.event-info').addClass('animate-in');
-        $('.action').addClass('animate-in');
-        $('.envelope').addClass('animate-in');
-      });
-    }, 1000);
+    $('.loader').animate({opacity:0}, function() {
+      $(this).remove();
+      $('.card-image-holder').addClass('animate-in');
+      $('.event-info').addClass('animate-in');
+      $('.action').addClass('animate-in');
+      $('.envelope').addClass('animate-in');
+    });
   }
 });
